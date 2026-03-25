e7cbafa86e4deb1e9dda644e3246e73a573ba1ac
umid-weeb <isroilov0705@gmail.com>
2026-03-25 19:07:05 +0500

    Vite-config

--- arena/vite.config.js
+++ arena/vite.config.js
@@ -1,35 +1,19 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "node:path";
-
-// export default defineConfig(({ command }) => ({
-//   plugins: [react()],
-//   base: command === "build" ? "/arena-spa/" : "/",
-//   publicDir: false,
-//   build: {
-//     outDir: path.resolve(__dirname, "../public/arena-spa"),
-//     emptyOutDir: true,
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server: {
-//     port: 5174,
-//   },
-// }));
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  
-  // 🔥 MUHIM
-  base: command === "build" ? "/zone/" : "/",
-
+  base: "/", // ✅ MUHIM
   build: {
-    outDir: "dist",
+    outDir: "dist", // ✅ standart
   },
-}));
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5174,
+  },
+});
