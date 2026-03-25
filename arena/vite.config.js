2864cb7854a97ae980d8a3afd4d47f6a0c58735a
umid-weeb <isroilov0705@gmail.com>
2026-03-25 19:27:17 +0500

    final fix deploy

--- arena/vite.config.js
+++ arena/vite.config.js
@@ -4,16 +4,13 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ MUHIM
+  base: "/",
   build: {
-    outDir: "dist", // ✅ standart
+    outDir: "dist",
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 5174,
   },
 });
